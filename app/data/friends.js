943ba90527313f860235e1e3d4a987abfad20f23
unknown <Alistair>
2019-05-10 20:21:26 -0400

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Albert Einstein',
+		photo: 'https://pbs.twimg.com/profile_images/879355674957926400/VSGZHGib.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Arnold Schwarzenegger',
+		photo: 'https://images.immediate.co.uk/volatile/sites/3/2011/07/GettyImages-614191110-b9ba743.jpg?quality=45&crop=4px,98px,2388px,1591px&resize=620,413',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Joan Rivers',
+		photo: 'https://ichef.bbci.co.uk/news/660/media/images/77386000/jpg/_77386457_joanrivers2.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Donald Trump',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-EnXpHh92jiF2TxW_jby5z-4VvXUS_E6_9ozKPfx_Qc6CxCaS',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Mike Pence',
+		photo: 'https://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2019/05/10/931/524/694940094001_6035104990001_6035114005001-vs.jpg?ve=1&tl=1',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Ed Kemper',
+		photo: 'https://www.thewrap.com/wp-content/uploads/2018/09/KemperonKemper.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ted Cruz',
+		photo: 'https://images.newrepublic.com/acb37554a1d596075604397e3871094d6a94d6fb.jpeg?w=1200&q=65&dpi=2.625&fm=pjpg&h=853',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Monica Lewinsky',
+		photo: 'https://www.kataeb.org/thumbnaile/crop/800/465/archive/today_news/mix/bill_clinton.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Paula Deen',
+		photo: 'https://i.ytimg.com/vi/ASxK6MalHwk/hqdefault.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'DJ Khaled',
+		photo: 'https://www.memesmonkey.com/images/memesmonkey/81/8160992ae731646c770f735fc2a46cde.jpeg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
